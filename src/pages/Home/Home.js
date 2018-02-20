8181d90378b6266259956fabd4f133f907287de9
Alexander Kozlov <alexabderl@coalla.ru>
2018-02-20 21:50:28 +0300

    fl# add my first styled component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,32 @@
 import React, {Component} from 'react';
+import styled from 'styled-components';
 
-const title = 'Home';
+// Create a <Title> react component that renders an <h1> which is
+// centered, palevioletred and sized at 1.5em
+const TitleH1 = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+// Create a <Wrapper> react component that renders a <section> with
+// some padding and a papayawhip background
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+const title = 'Hello World, this is my first styled component!';
 
 class Home extends Component {
     render() {
         document.title = title;
 
         return (
-            <div>
-                <h1>{title}</h1>
-            </div>
+            // Use them like any other React component – except they're styled!
+            <Wrapper>
+                <TitleH1>{title}</TitleH1>
+            </Wrapper>
         )
     }
 }
